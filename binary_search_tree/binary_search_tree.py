360257422543454680a734fca1df622fece88ea9
Jeremy Morales <jeremy.morales@gmail.com>
2020-05-27 16:27:05 -0700

    contains / get max completed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,16 +17,56 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        current = self
+        while current:
+            if current.value > value:
+                if current.left is None:
+                    current.left = BSTNode(value)
+                    return
+                current = current.left
+            elif current.value <= value:
+                if current.right is None:
+                    current.right = BSTNode(value)
+                    return
+                current = current.right
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        current = self
+       
+        
+        while current:
+            if current.value == target:
+                return True
+            elif current.value > target:
+                if current.left is None:
+                    return False
+                current = current.left
+            elif current.value < target:
+                if current.right is None:
+                    return False
+                current = current.right
+                
+                    
+
+
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current = self
+        max_value = self.value
+
+        while current:
+            if current.right is None:
+                max_value = current.value
+                return max_value
+            current = current.right
+
+        
+        return max_value
+
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
@@ -59,3 +99,11 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+# bst = BSTNode(5)
+
+# bst.insert(5)
+# bst.insert(2)
+# bst.insert(6)
+# print(bst.get_max())
